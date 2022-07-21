aed40c776c7fa393208730b8f17f9c512165b121
Sem <semik.kolesnikovv@gmail.com>
2022-07-21 13:20:26 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sem Koliesnikov on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
